f20c9d423d346e332b3ed5d2ed2eeb0c399f61b4
Haim-hub <niklas@haim.dk>
2023-11-30 11:35:52 +0800

    2023-11-30 contribution

--- README.md
+++ README.md
@@ -4,4 +4,44 @@ For those of us who wants to flex our GitHub contributions without actually havi
 
 ## What is this?
 
-## How to use
+This is a simple script that will automatically create commits on your GitHub account. It will create a commit for each day of the year, and each commit will have a random number of files, each with a random number of lines.
+
+## Why?
+
+Because I wanted to flex my GitHub contributions without actually having contributed much. And also to see if it was poopible.
+
+## How to use?
+
+To use this script, you need to have Python 3 installed. You can install it by running `pip3 install requests` in your terminal.
+You also need to have Git installed. You can install it by running `pip3 install GitPython` in your terminal.
+
+1. Fork this repository.
+2. Clone your forked repository.
+3. Run `python3 autoflex.py` in the cloned repository.
+
+### Auto Run
+
+If you want to run this script automatically, there a a lot of ways to do so, and I will not go into detail on how to do it. But here are some suggestions:
+
+#### Windows
+
+If you are on Windows, you can use Task Scheduler to run the script daily. I don't use Windows myself, so this might be wrong, feel free to correct me.
+
+1. Open Task Scheduler and create a new task.
+2. Set the trigger to run daily at your preferred time.
+3. For the action, start a program, and browse to your Python executable (python.exe), usually located in the Python installation directory.
+4. In the arguments, enter the path to your Python script.
+
+#### Linux/MacOS
+
+For Linux and MacOS, you can use cron jobs to run the script daily.
+
+1. Open your terminal.
+2. Type crontab -e to edit the cron jobs.
+3. Add a line like this to execute the script daily at a specific time (e.g., 3 AM):
+
+```ruby
+0 3 * * * /usr/bin/python /path/to/your/example.py
+```
+
+4. Save and exit the editor.
--- main.py
+++ main.py
@@ -1,52 +1,68 @@
 import random
 from git import Repo
 import os
+import requests
 
-# Path to your repository
-repo = Repo(os.getcwd())
+def check_internet_connection(url='http://www.github.com', timeout=5):
+    try:
+        # Send a request to the specified URL
+        response = requests.get(url, timeout=timeout)
+        # If the request was successful, return True
+        return response.status_code == 200
+    except requests.ConnectionError:
+        # If a connection error occurs, return False
+        return False
 
-#Get The Date
-date = repo.git.log('-1', '--format=%cd', '--date=format:%Y-%m-%d') 
+if check_internet_connection():
+    print("Internet connection is available.")
+    # Path to your repository
+    repo = Repo(os.getcwd())
 
-# Git pull
-origin = repo.remote(name='origin')
-origin.pull()
+    #Get The Date
+    date = repo.git.log('-1', '--format=%cd', '--date=format:%Y-%m-%d') 
 
-# Get Random Number for the day
-rd = random.randint(1, 10)
-
-for i in range(rd):
-    number = 0
-    file_path = 'main.txt'
-    # Read file
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-        last_line = lines[-1]
-        last_word = last_line.split()[-1]
-        try:
-            number = int(last_word)
-            # Use 'number' in your script
-        except ValueError:
-            print(f"The last word '{last_word}' is not a number.")
-
-    # Clear file
-    with open(file_path, 'w') as file:
-        # Write new content to the file
-        file.write("")
-
-    # Change File
-    with open(file_path, 'a') as f:
-        f.write('Hello World! \n')
-        f.write('Today is ' + date + 'add I have made a contribution \n')
-        f.write('Total number of automatic contributions is: ' + str(number + 1))
-
-
-    # Add all changes
-    repo.git.add(A=True)
-
-    # Commit
-    repo.index.commit(date + ' contribution')
-
-    # Push
+    # Git pull
     origin = repo.remote(name='origin')
-    origin.push()
+    origin.pull()
+
+    # Get Random Number for the day
+    rd = random.randint(1, 10)
+
+    for i in range(rd):
+        number = 0
+        file_path = 'main.txt'
+        # Read file
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+            last_line = lines[-1]
+            last_word = last_line.split()[-1]
+            try:
+                number = int(last_word)
+                # Use 'number' in your script
+            except ValueError:
+                print(f"The last word '{last_word}' is not a number.")
+
+        # Clear file
+        with open(file_path, 'w') as file:
+            # Write new content to the file
+            file.write("")
+
+        # Change File
+        with open(file_path, 'a') as f:
+            f.write('Hello World! \n')
+            f.write('Today is ' + date + ' and I have made a contribution \n')
+            f.write('Total number of automatic contributions is: ' + str(number + 1))
+
+
+        # Add all changes
+        repo.git.add(A=True)
+
+        # Commit
+        repo.index.commit(date + ' contribution')
+
+        # Push
+        origin = repo.remote(name='origin')
+        origin.push()
+
+else:
+    print("No internet connection. Please check your connection.")
