d8306203d7ddaa33bbf13f135d132a512eb662b8
Haim-hub <niklas@haim.dk>
2023-11-30 10:55:48 +0800

    2023-11-30contribution

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AutoFlex
+
 For those of us who wants to flex our GitHub contributions without actually having contributed much.
+
+## What is this?
+
+## How to use
--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import os
 repo = Repo(os.getcwd())
 
 #Get The Date
-date = repo.git.log('-1', '--format=%cd')
+date = repo.git.log('-1', '--format=%cd', '--date=format:%Y-%m-%d')
 
+number = 0
+# Read file
+with open('main.txt', 'r') as f:
+    lines = f.readlines()
+    last_line = lines[-1]
+    number = int(last_line.split()[-1])
 
 # Change File
 with open('main.txt', 'a') as f:
     f.write('Hello World! \n')
     f.write('Today is ' + date + 'add I have made a contribution')
+    f.write('Total number of automatic contributions is:' + str(number + 1))
 
 
 # Add all changes
