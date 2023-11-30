0ccfa48df6ee3a30342e83aa0d66e01d3c44b253
Haim-hub <niklas@haim.dk>
2023-11-30 10:49:17 +0800

    Thu Nov 30 10:44:04 2023 +0800contribution

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import os
 # Path to your repository
 repo = Repo(os.getcwd())
 
+#Get The Date
+date = repo.git.log('-1', '--format=%cd')
+
+
+# Change File
+with open('main.txt', 'a') as f:
+    f.write('Hello World! \n')
+    f.write('Today is ' + date + 'add I have made a contribution')
+
+
 # Add all changes
 repo.git.add(A=True)
 
 # Commit
-repo.index.commit('Test Commit')
+repo.index.commit(date + 'contribution')
 
 # Push
 origin = repo.remote(name='origin')
