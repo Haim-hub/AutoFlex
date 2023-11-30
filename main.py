934d3a923b6b537fac3f52bfa7eeef375c20db4c
Haim-hub <niklas@haim.dk>
2023-11-30 14:01:19 +0800

    2023-11-30 contribution

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-#!/opt/homebrew/bin/python3
+#!/opt/homebrew/bin/pip3
 import random
 from git import Repo, GitCommandError
-import os
+#
 import requests
 
-def check_internet_connection(url='http://www.github.com', timeout=5):
-    try:
-        # Send a request to the specified URL
-        response = requests.get(url, timeout=timeout)
-        # If the request was successful, return True
-        return response.status_code == 200
-    except requests.ConnectionError:
-        # If a connection error occurs, return False
-        return False
+# def check_internet_connection(url='http://www.github.com', timeout=5):
+#     try:
+#         # Send a request to the specified URL
+#         response = requests.get(url, timeout=timeout)
+#         # If the request was successful, return True
+#         return response.status_code == 200
+#     except requests.ConnectionError:
+#         # If a connection error occurs, return False
+#         return False
 
-if check_internet_connection():
+if True:
     print("Internet connection is available.")
 
     try:
+        print("Trying to push to GitHub...")
         # Path to your repository
         repo = Repo("/Users/niklashaim/Documents/GitHub_Flex/AutoFlex")
 
@@ -66,6 +67,7 @@ if check_internet_connection():
             # Push
             origin = repo.remote(name='origin')
             origin.push()
+            print("Pushed to GitHub successfully.")
     except GitCommandError as e:
         print(f"An error occurred: {e}")    
 
